dd1b5b0444f48a47926e9c28bcc257eed178ac01
serg-gavel <gavelyuk.sergey.13@gmail.com>
2019-10-02 19:23:45 +0300

    Update *

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,55 @@
-const path = require('path')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        filename: '[].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: './dist'
-    }
-}
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: "../",
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { sourceMap: true } },
+          { loader: "sass-loader", options: { sourceMap: true } }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    overlay: true,
+    liveReload: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+      ignoreOrder: false // Enable to remove warnings about conflicting order
+    })
+  ]
+};
